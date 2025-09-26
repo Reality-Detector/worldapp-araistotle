0ef57500bc2411f1a51fcd25ea6783d53622a0b4
drwavy <contact@drwavy.com>
2025-09-26 19:15:45 -0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { VerifyBlock } from "@/components/Verify";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
+      
       <SignIn />
       <VerifyBlock />
       <PayBlock />
