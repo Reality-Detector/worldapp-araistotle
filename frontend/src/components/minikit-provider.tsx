40c8b6ddb676939bf373fb0c2ce1734e7c30731d
drwavy <contact@drwavy.com>
2025-09-25 21:41:59 -0700

    Setup, Ngrok, Nginx, backend, frontend, service file, env setup, basic Node.js HTTP server

--- frontend/src/components/minikit-provider.tsx
+++ frontend/src/components/minikit-provider.tsx
@@ -1,11 +1,8 @@
-"use client"; // This isn't needed for your project, but it's harmless
-
 import { ReactNode, useEffect } from "react";
 import { MiniKit } from "@worldcoin/minikit-js";
 
 export default function MiniKitProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
-    // The install function expects an object like { app_id: "..." }
     MiniKit.install({
       app_id: "app_31a60f60d27f7ae23f866444152cfad0",
     });
